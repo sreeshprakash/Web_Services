9c93497572bab302804a11c3763bde5f856f755c
Sreesh Prakash <sprakas3@asu.edu>
2015-11-05 22:50:48 -0700

    Console application update

--- Hospital Management System/ConsoleApplication11/ConsoleApplication11/Program.cs
+++ Hospital Management System/ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//unused console
 namespace ConsoleApplication11
 {
     class Program
     {
         static void Main(string[] args)
         {
+			
         }
     }
 }
